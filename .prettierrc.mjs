6307a5d01a5026be527550d098e49dc4af92d357
John Champ <72492771+john-champ@users.noreply.github.com>
2025-02-14 09:17:54 +0200

    Deps updated + Prettier improvement

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,7 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
+  tailwindStylesheet: "./src/assets/css/tailkit.css",
   plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
